c27a3fff7169e33781f97f2210a55fa261c7515a
Mohammad Sohail <Mohammadsohail099@gmail.com>
2020-11-20 17:50:52 +0530

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+
+const App = () => {
+
+    
+      const [quote,setQuote] = useState('')
+
+   console.log(quote);
+
+  const RandomdigitPicker = ()=>{
+          
+        return Math.floor(Math.random()*100)
+
+    }
+
+     const quotefetch = async (func)=>{
+
+        const res = await axios.get('https://type.fit/api/quotes')
+
+        const quotes = res.data;
+
+        setQuote(quotes[func()])
+         
+        
+
+    }
+
+
+
+const randomColor = ()=>{
+
+ const r = Math.floor(Math.random()*255)
+ const g = Math.floor(Math.random()*255)
+ const b = Math.floor(Math.random()*255)
+
+const body = document.body 
+const quote = document.getElementById('quote');
+body.style.background = `rgb(${r},${g},${b})`
+body.style.transition= 'background 0.6s'
+quote.style.color = `rgb(${r},${g},${b})`
+
+
+
 }
+
+useEffect(()=>{
+
+     randomColor()
+    quotefetch(RandomdigitPicker)
+
+},[])
+
+
+
+  
+ return <div className="ui container">
+   
+<div className="ui centered grid" style={{marginTop: 60}} >
+ <div className="ui segment" style={{padding:60,fontSize:20, borderRadius:6}}>
+  <div className="ui header">Random Quote's</div> 
+  
+ <cite id="quote"> {`"${quote.text}"`}</cite> - <span>{quote.author}</span>
+  
+
+
+
+           
+<div style={{textAlign:"right",marginTop:10}} >
+    <button className="ui green basic button" onClick={()=>{
+     randomColor()
+     quotefetch(RandomdigitPicker);
+    }} >Generate quote</button>
+    </div>
+  
+
+
+
+ </div>
+</div>
+ </div>
+
+
+
+
+
+
+
+}
+
+
+
 
 export default App;
